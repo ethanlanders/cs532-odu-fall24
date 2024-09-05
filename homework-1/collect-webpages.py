286ba3082575f1b155f3ceeaaa9c2cc0cef07d4b
Ethan Landers <eland007@odu.edu>
2024-09-05 15:21:49 -0400

    uploading image, possible change to .py

---
+++ homework-1/collect-webpages.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import sys
+
+def main(seed_uri):
+    pass
+
+def extract_links():
+    pass
+
+def save_uris_to_file():
+    # Save the list of URIs to a text file
+    pass
+
+def is_valid_html_page(uri):
+    pass
+
+# Start the main process
+main(sys.argv[1])
