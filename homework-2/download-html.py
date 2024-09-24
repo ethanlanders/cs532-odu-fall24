2972c6fa21781be9c60a7f30428f133be902ec53
Ethan Landers <eland007@odu.edu>
2024-09-24 18:23:49 -0400

    implemented download-html.py, uri-mapping.txt filled out

--- homework-2/download-html.py
+++ homework-2/download-html.py
@@ -1,5 +1,7 @@
 import requests
 import hashlib
+import os
+import shutil
 
 uris=[]
 
@@ -10,6 +12,33 @@ with open("homework-1/output/uris.txt", 'r', encoding='utf-8') as file:
 # Clean up the URIs by stripping any extra whitespace/newline characters
 uris = [uri.strip() for uri in uris]
 
-print(f"Loaded {len(uris)} URIs.")
+print(f"Loaded {len(uris)} URIs.\n")
 
 uri_hash_map = {}
+
+if os.path.exists('raw_html'):
+    print("raw_html directory exists. Deleting all files within it.\n")
+    shutil.rmtree('raw_html')
+    os.makedirs("raw_html")
+else:
+    os.makedirs("raw_html")
+
+for uri in uris:
+    hash_object = hashlib.md5(uri.encode())
+    filename = f"{hash_object.hexdigest()}.html"
+
+    try:
+        response = requests.get(uri, timeout=5)
+        with open(f"raw_html/{filename}", 'w', encoding='utf-8') as file:
+            file.write(response.text)
+
+        uri_hash_map[filename] = uri
+
+    except requests.exceptions.RequestException as e:
+        print(f"Error fetching {uri}: \n{e}\n")
+
+with open('uri_mapping.txt', 'w', encoding='utf-8') as f:
+    for filename, uri in uri_hash_map.items():
+        f.write(f"{filename}: {uri}\n")
+
+print("HTML files and URI mappings have been successfully saved.\n")
