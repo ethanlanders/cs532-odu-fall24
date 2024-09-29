1fd931ef44341b8cfcc464c59b5f7baf02625064
Ethan Landers <eland007@odu.edu>
2024-09-29 11:58:30 -0400

    fixed sorting in compute-tfidf.py, added comments to pagerank.py

--- homework-2/compute-tfidf.py
+++ homework-2/compute-tfidf.py
@@ -1,21 +1,27 @@
 import math
 import os
 
+# Function to compute Term Frequency (TF)
 def compute_tf(word, document):
-    words = document.split()
-    return words.count(word) / len(words)
+    words = document.split() # Split document into words (tokens)
+    return words.count(word) / len(words) 
 
+# Function to compute Inverse Document Frequency (IDF)
 def compute_idf(word, documents, total_docs_in_directory):
     num_docs_with_word = 0
     
+    # Loop through each document to check if the word exists
     for doc in documents:
         if word in doc:
             num_docs_with_word += 1
+    
+    # Compute the IDF using the total number of document and number off docs with term
     return math.log((total_docs_in_directory) / (num_docs_with_word), 2)
 
+# Define the query term for which we want to calculate TF-IDF
 query_term = "climate"
 
-# Ten files that contain the query term (got with grep)
+# List of 10 files that contain the query term (found using grep)
 file_uris = [
     'homework-2/processed_html/3ac6cb5edbed898516d4a54c7af9e22d.html',
     'homework-2/processed_html/3ba3eee197e9cd3a3851eb90582df2dd.html',
@@ -29,25 +35,33 @@ file_uris = [
     'homework-2/processed_html/90a687c0ed013c36ee44a64c87ba55d6.html'
 ]
 
+# Path to the directory containing the processed HTML files
 directory_path = 'homework-2/processed_html'
+
+# Get the total number of documents (processed HTML files) in the directory
 total_docs_in_directory = len([doc for doc in os.listdir(directory_path)])
 
 # Dictionary to store the content of each file
 documents = {}
 
+# Read the content of each file and store it in the 'documents' dictionary
 for uri in file_uris:
     with open(uri, 'r', encoding='utf-8') as file:
         documents[uri] = file.read()
 
+# List to store the computed TF-IDF results for each file
 tf_idf_results = []
 
+# Loop through each document, compute TF, IDF, and TF-IDF, and store the results
 for uri, document in documents.items():
     tf = compute_tf(query_term, document)
     idf = compute_idf(query_term, documents.values(), total_docs_in_directory)
     tf_idf = tf * idf
     tf_idf_results.append((tf_idf, tf, idf, uri))
 
-tf_idf_results.sort(key=lambda x: x[3], reverse=True)
+# Sort the results by the TF-IDF
+tf_idf_results.sort(key=lambda x: x[0], reverse=True)
 
+# Print out the TF-IDF, TF, IDF values along with the file URI for each document
 for result in tf_idf_results:
     print(f"TF-IDF: {result[0]:.4f}, TF: {result[1]:.4f}, IDF: {result[2]:.4f}, URI: {result[3]}\n")
--- homework-2/pagerank.py
+++ homework-2/pagerank.py
@@ -1,4 +1,10 @@
-# Original PageRank values from https://searchenginereports.net/google-pagerank-checker
+"""
+References:
+- Data Normalization Techniques: https://analystanswers.com/data-normalization-techniques-easy-to-advanced-the-best/
+"""
+
+# PageRank values (manually obtained) from https://searchenginereports.net/google-pagerank-checker
+# For the ten URIs that have the query term
 pagerank_values = {
     "https://www.odu.edu/oir": 7,
     "https://news-un-org.translate.goog/en/?_x_tr_sl=en&_x_tr_tl=ru": 6,
@@ -12,20 +18,25 @@ pagerank_values = {
     "https://www-leibniz--gemeinschaft-de.translate.goog/en/about-us/whats-new/news?_x_tr_sl=en&_x_tr_tl=uk": 0,
 }
 
-# Get the minimum and maximum PageRank values
+# Get the minimum and maximum PageRank values from the dictionary
+# These values are used for normalization to scale the PageRank values
 min_pr = min(pagerank_values.values())
 max_pr = max(pagerank_values.values())
 
-# Normalize the PageRank values
-# https://analystanswers.com/data-normalization-techniques-easy-to-advanced-the-best/
+# Normalize the PageRank values using min-max normalization
+# Formula for normalization: (value - min) / (max - min)
 normalized_pagerank = {}
-for uri, pr in pagerank_values.items():
-    if max_pr != min_pr:
-        normalized_value = (pr - min_pr) / (max_pr - min_pr)
-    else:
-        normalized_value = 0
 
+# Loop through the PageRank values and normalize each one
+for uri, pr in pagerank_values.items():
+    if max_pr != min_pr: # Check to avoid division by zero
+        normalized_value = (pr - min_pr) / (max_pr - min_pr) # Normalize the PageRank value
+    else:
+        normalized_value = 0 # If max and min are the same, set the normalized value to 0
+
+    # Store the normalized value in the new dictionary with the URI as the key
     normalized_pagerank[uri] = normalized_value
 
+# Print the URI along with the normalized PageRank values
 for uri, norm_pr in normalized_pagerank.items():
     print(f"URI: {uri}, Normalized PageRank: {norm_pr:.1f}\n")
