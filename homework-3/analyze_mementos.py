e6d4903c13a31fd33a85d7b11f47d6eadbb8c4a0
Ethan Landers <eland007@odu.edu>
2024-10-09 16:22:34 -0400

    first push for HW3, setting up necessary files for assignment

---
+++ homework-3/analyze_mementos.py
@@ -0,0 +1,8 @@
+def load_timemap(file):
+    pass
+
+def count_mementos(timemap):
+    pass
+
+def generate_table(memento_counts):
+    pass
---
+++ homework-3/download_timemaps.py
@@ -0,0 +1,7 @@
+path = "homework-1/output/uris.txt"
+
+def query_memgator(uri):
+    pass
+
+def save_timemap(filename, data):
+    pass
