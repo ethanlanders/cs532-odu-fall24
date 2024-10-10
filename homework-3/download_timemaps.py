8f6607bf4b4b345aa50ce7ff25147dc6022bd9bf
Ethan Landers <eland007@odu.edu>
2024-10-10 12:11:50 -0400

    TimeMaps obtained for first 50 URIs

--- homework-3/download_timemaps.py
+++ homework-3/download_timemaps.py
@@ -2,8 +2,6 @@ import subprocess # https://docs.python.org/3/library/subprocess.html
 import time
 import os
 
-path = "homework-1/output/uris.txt"
-
 def load_uri_mapping(mapping_file):
     uri_hash_mapping = {}
     with open(mapping_file, 'r') as f:
@@ -19,8 +17,12 @@ def query_memgator(uri, output_file):
     if result.returncode != 0:
         print(f"Error querying MemGator for {uri}: {result.stderr}")
     else:
-        with open(output_file, 'w') as f:
-            f.write(result.stdout)
+        if result.stdout != None:
+            print(f"Raw output for {uri}: {result.stdout}")
+            with open(output_file, 'w') as f:
+                f.write(result.stdout)
+        else:
+            print(f"No raw output for {uri}.")
 
     # Add sleep to avoid overwhelming the server
     time.sleep(10)
@@ -31,13 +33,15 @@ def download_timemap(uri_mapping_file, output_dir):
     
     uri_hash_map = load_uri_mapping(uri_mapping_file)
 
-    # Limit to first 3 items for testing
-    test_uri_hash_map = dict(list(uri_hash_map.items())[:3])
+    # Testing for 
+    test_uri_hash_map = dict(list(uri_hash_map.items())[50:])
+
+    start=50
 
     for hash_file, uri in test_uri_hash_map.items():
         output_file = os.path.join(output_dir, f"{hash_file}.json")
-        print(f"Querying TimeMap for {uri} and saving as {output_file}")
+        print(f"\nQuerying TimeMap for {uri} and saving as {output_file}")
         query_memgator(uri, output_file)
 
-# TODO: The three generated timemaps/ directory files are empty. Why?
+
 download_timemap("homework-2/uri_mapping.txt", "homework-3/timemaps")
